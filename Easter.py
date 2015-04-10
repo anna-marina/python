3b35a16d03a3cce0d2b814cc7cef0009b849c377
anna-marina <maryina.anna.va@gmail.com>
2015-04-10 18:42:47 +0300

    Create Easter.py
    calculator date of Easter

---
+++ Easter.py
@@ -0,0 +1,14 @@
+year = input()
+year = int (year)
+a = (19 * (year % 19) + 15) % 30
+b = (2 * (year % 4) + 4 * (year % 7) + 6 * a + 6) % 7
+if a + b > 9:
+    t = a + b - 9
+    if (t + 13 < 31):
+        print ( "the", t + 13, "of" , "April")
+    else:
+        print ( "the", t + 13 - 30, "of" , "May")
+    
+else:
+    t = 22 + a + b + 13 - 31
+    print ( "the", t, "of" , "April")
