95ec8837b89a89c1cf323b7e2748f2e4cdcbf4a7
anna-marina <maryina.anna.va@gmail.com>
2015-04-10 22:08:26 +0300

    Update Easter.py
    some correction

--- Easter.py
+++ Easter.py
@@ -1,5 +1,4 @@
-year = input()
-year = int (year)
+year = int(input())
 a = (19 * (year % 19) + 15) % 30
 b = (2 * (year % 4) + 4 * (year % 7) + 6 * a + 6) % 7
 if a + b > 9:
@@ -8,7 +7,6 @@ if a + b > 9:
         print ( "the", t + 13, "of" , "April")
     else:
         print ( "the", t + 13 - 30, "of" , "May")
-    
 else:
     t = 22 + a + b + 13 - 31
     print ( "the", t, "of" , "April")
